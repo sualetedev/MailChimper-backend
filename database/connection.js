9dae292198e99520483e2aa478ac0af4251ecba3
Alejandro Izquierdo <sualetedev@gmail.com>
2025-05-15 09:41:28 +0200

    bcryptjs y docker error

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connection = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/mailchimper')
+        await mongoose.connect('mongodb://mongo:27017/mailchimper' || 'mongodb://localhost:27017/mailchimper')
     } catch ( error ) {
         console.log("Error al conectar a la base de datos:", error);
     }
